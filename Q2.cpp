eba7901c99a18f03cc167ea4311cabf79b34be50
mems250005031-collab <mems250005031@iiti.ac.in>
2025-10-17 16:28:31 +0530

    Update Q2.cpp

--- Q2.cpp
+++ Q2.cpp
@@ -1,31 +1,31 @@
 #include <iostream>
 using namespace std;
 
-// Define the structure 'Color'
+
 struct Color {
-    int red;   // Red component of the color
-    int green; // Green component of the color
-    int blue;  // Blue component of the color
+    int red;   
+    int green; 
+    int blue;  
 };
 
 int main() {
-    // Declare and initialize an array of 5 'Color' structures with sample RGB values
+    
     Color colors[5] = {
-        {255, 0, 0},   // Red color
-        {0, 255, 0},   // Green color
-        {0, 0, 255},   // Blue color
-        {255, 255, 0}, // Yellow color
-        {0, 255, 255}  // Cyan color
+        {255, 0, 0},   
+        {0, 255, 0},   
+        {0, 0, 255},   
+        {255, 255, 0}, 
+        {0, 255, 255}  
     };
 
-    // Use a 'for' loop to invert the color values of the first 3 elements
+    
     for (int i = 0; i < 3; i++) {
         colors[i].red = 255 - colors[i].red;
         colors[i].green = 255 - colors[i].green;
         colors[i].blue = 255 - colors[i].blue;
     }
 
-    // Display the new RGB values for all 5 elements
+    
     cout << "Inverted Colors:" << endl;
     for (int i = 0; i < 5; i++) {
         cout << "Color " << i + 1 << " - R: " << colors[i].red 
